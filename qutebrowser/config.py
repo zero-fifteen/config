08a9efa39c36219f2a284f13e5b7d18adbc9413d
fifteen <fifteen@fifteen.fifteen>
2020-10-04 21:14:30 -0400

    20201004

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -14,6 +14,8 @@ def dict_attrs(obj, path=''):
 for k, v in dict_attrs(yaml_data):
     config.set(k, v)
 
+config.bind("xf", "hint links spawn mpv {hint-url}")
+
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
